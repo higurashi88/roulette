8baa8cb67115ab1fff6a0d7de6508000917e3c12
higurashi88 <msippc1997@gmail.com>
2024-03-16 22:12:30 +0900

    fix default value -> placeholder

--- roulette.js
+++ roulette.js
@@ -4,6 +4,10 @@ let number = document.getElementById('number');
 // class="item"の要素を取得
 let boxes = document.querySelectorAll('.item');
 
+// 回る間隔
+let span = 100;
+let stop_interval = 0;
+
 // 現在のインデックス
 let currentIndex = 0;
 
@@ -20,7 +24,7 @@ number.addEventListener('change', function () {
             let input = document.createElement('input');
             input.classList.add('culum');
             input.setAttribute('type', 'text');
-            input.setAttribute('value', '項目' + (i + 1));
+            input.setAttribute('placeholder', '項目' + (i + 1));
             div.appendChild(input);
             roulette_area.appendChild(div);
         }
@@ -41,7 +45,7 @@ function highlightBox() {
         if (is_start) {
             highlightBox();
         }
-    }, 100);
+    }, span);
 }
 
 // ルーレットを開始する
@@ -55,8 +59,14 @@ function startRoulette() {
 
 // ルーレットを停止する
 function stopRoulette() {
-    document.getElementById('start').disabled = false;
-    is_start = false;
+    //stop_intervalミリ秒だけ待ってからルーレットを停止する
+    stop_interval = Math.floor(Math.random() * 2000) + 750;
+    span = 250;
+    setTimeout(() => {
+        is_start = false;
+        document.getElementById('start').disabled = false;
+    }, stop_interval);
+
 }
 
 // ルーレットをリセットする
